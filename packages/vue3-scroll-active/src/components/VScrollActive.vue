6e91aa53c138ce7f69431a9a4cbb60fe2ab623a4
QUALIA_ZX <QUALIA_ZX@outlook.com>
2023-04-11 15:30:36 +0800

    feat: 更新vue3版本组件

---
+++ packages/vue3-scroll-active/src/components/VScrollActive.vue
@@ -0,0 +1,51 @@
+<template>
+  <div ref="refScrollActive">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+/* 修改自v-scroll-active */
+import { ref, onBeforeUpdate, onUpdated, onBeforeUnmount, onMounted, nextTick } from 'vue';
+import ScrollActive from 'scroll-active';
+
+const props = defineProps({
+  activeClass: { type: String, default: 'active' },
+  offset: { type: [Number, String], default: 0 },
+  hash: { type: Boolean, default: false },
+});
+const emit = defineEmits(['update']);
+
+const instanceScrollActive = ref();
+const refScrollActive = ref(null);
+
+const initScrollActive = () => {
+  instanceScrollActive.value = new ScrollActive({
+    wrapper: refScrollActive.value,
+    activeClass: props.activeClass,
+    offset: Number(props.offset),
+    hash: props.hash,
+    update: (id) => {
+      console.log(id);
+      emit('update', id);
+    },
+  });
+};
+
+// 释放资源
+const destroyScrollActive = () => {
+  if (instanceScrollActive.value) {
+    instanceScrollActive.value.dispose();
+    instanceScrollActive.value = void 0;
+  }
+};
+
+onMounted(async () => {
+  await nextTick();
+  initScrollActive();
+});
+
+onBeforeUpdate(() => destroyScrollActive());
+onBeforeUnmount(() => destroyScrollActive());
+onUpdated(() => initScrollActive());
+</script>
